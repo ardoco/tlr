06f3149d17dd7f69b99339c85fc2e3a7a5456298
Sophie Corallo <sophie.cata.schulz@web.de>
2025-01-02 11:13:59 +0100

    update

--- tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/tlr/tests/integration/tlrhelper/ModelElementSentenceLink.java
+++ tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/tlr/tests/integration/tlrhelper/ModelElementSentenceLink.java
@@ -3,21 +3,21 @@ package edu.kit.kastel.mcse.ardoco.tlr.tests.integration.tlrhelper;
 
 import java.util.Comparator;
 
-import edu.kit.kastel.mcse.ardoco.core.api.entity.ArchitectureEntity;
+import edu.kit.kastel.mcse.ardoco.core.api.entity.Entity;
+import edu.kit.kastel.mcse.ardoco.core.api.stage.connectiongenerator.SadModelTraceLink;
 import edu.kit.kastel.mcse.ardoco.core.api.text.SentenceEntity;
-import edu.kit.kastel.mcse.ardoco.core.api.tracelink.SadSamTraceLink;
 
 /**
  * Represents a simple trace link by the id of the model and number of the sentence involved.
  */
 public record ModelElementSentenceLink(String modelElementId, int sentenceNumber) implements Comparable<ModelElementSentenceLink> {
 
-    public ModelElementSentenceLink(SadSamTraceLink traceLink) {
-        this(traceLink.getArchitectureEntityId(), traceLink.getSentenceNumber());
+    public ModelElementSentenceLink(SadModelTraceLink traceLink) {
+        this(traceLink.getEntityId(), traceLink.getSentenceNumber());
     }
 
-    public ModelElementSentenceLink(SentenceEntity sentence, ArchitectureEntity architectureEntity) {
-        this(architectureEntity.getId(), sentence.getSentence().getSentenceNumber());
+    public ModelElementSentenceLink(SentenceEntity sentence, Entity entity) {
+        this(entity.getId(), sentence.getSentence().getSentenceNumber());
     }
 
     @Override
