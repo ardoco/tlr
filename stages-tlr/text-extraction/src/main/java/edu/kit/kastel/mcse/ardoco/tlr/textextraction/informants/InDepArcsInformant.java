cf1ea1ca50d4b5b734008d7013d05871c0a82ed4
Dominik Fuchß <develop@fuchss.org>
2025-07-17 18:47:34 +0200

    fix config tests

--- stages-tlr/text-extraction/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/textextraction/informants/InDepArcsInformant.java
+++ stages-tlr/text-extraction/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/textextraction/informants/InDepArcsInformant.java
@@ -20,10 +20,10 @@ import edu.kit.kastel.mcse.ardoco.core.data.DataRepository;
 public class InDepArcsInformant extends TextExtractionInformant {
 
     @Configurable
-    private final double nameOrTypeWeight = 0.5;
+    private double nameOrTypeWeight = 0.5;
 
     @Configurable
-    private final double probability = 1.0;
+    private double probability = 1.0;
 
     /**
      * Prototype constructor.
