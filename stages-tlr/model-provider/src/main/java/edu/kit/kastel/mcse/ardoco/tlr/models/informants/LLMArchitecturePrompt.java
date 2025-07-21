aa646113260efa941f330547511f2cb3f14d20f2
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-07-21 17:25:20 +0200

    Rename prompts

--- stages-tlr/model-provider/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/models/informants/LLMArchitecturePrompt.java
+++ stages-tlr/model-provider/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/models/informants/LLMArchitecturePrompt.java
@@ -4,7 +4,7 @@ package edu.kit.kastel.mcse.ardoco.tlr.models.informants;
 import java.util.List;
 
 public enum LLMArchitecturePrompt {
-    DOCUMENTATION_ONLY_V1(
+    EXTRACT_FROM_ARCHITECTURE(
             """
                     Your task is to identify the high-level components based on a software architecture documentation. In a first step, you shall elaborate on the following documentation:
 
@@ -16,7 +16,7 @@ public enum LLMArchitecturePrompt {
                     - Name1
                     - Name2
                     """), //
-    CODE_ONLY_V1(
+    EXTRACT_FROM_CODE(
             """
                     You get the {FEATURES} of a software project. Your task is to summarize the {FEATURES} w.r.t. the high-level architecture of the system. Try to identify possible components.
 
@@ -30,7 +30,7 @@ public enum LLMArchitecturePrompt {
                     - Name1
                     - Name2
                     """), //
-    AGGREGATION_V1("""
+    AGGREGATE("""
             You get a list of possible component names. Your task is to aggregate the list and remove duplicates.
             Omit common prefixes and suffixes in the names in camel case.
             Output format:
@@ -49,7 +49,7 @@ public enum LLMArchitecturePrompt {
     }
 
     public List<String> getTemplates() {
-        if (this == CODE_ONLY_V1)
+        if (this == EXTRACT_FROM_CODE)
             throw new IllegalArgumentException("This method is not supported for this enum value");
         return templates;
     }
