494fc69d7aa96686a9e0dfdd25b6aea8b62f672e
Jan Keim <jan.keim@kit.edu>
2025-07-30 18:04:16 +0200

    Update artemis evaluation

--- tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/tlr/tests/integration/ArtemisIT.java
+++ tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/tlr/tests/integration/ArtemisIT.java
@@ -1,40 +1,39 @@
 /* Licensed under MIT 2025. */
 package edu.kit.kastel.mcse.ardoco.tlr.tests.integration;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.kit.kastel.mcse.ardoco.core.common.util.Environment;
 import edu.kit.kastel.mcse.ardoco.tlr.tests.approach.ArtemisEvaluationProject;
 import edu.kit.kastel.mcse.ardoco.tlr.tests.integration.evaluation.ArtemisEvaluation;
 
 class ArtemisIT extends AbstractArdocoIT {
-
-    @BeforeAll
-    static void beforeAll() {
-        System.setProperty(LOGGING_ARDOCO_CORE, "info");
-        Assumptions.assumeTrue(Environment.getEnv("OPENAI_API_KEY") != null); // TODO
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.setProperty(LOGGING_ARDOCO_CORE, "error");
-    }
-
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+    
     @DisplayName("Evaluate ArTEMiS (SAD-SAM TLR with NER)")
     @ParameterizedTest(name = "{0}")
     @EnumSource(ArtemisEvaluationProject.class)
     void evaluateSadSamTlrIT(ArtemisEvaluationProject project) {
         Assumptions.assumeTrue(Environment.getEnv("CI") == null);
+        if (checkLlmProvision()) {
+            logger.info("Skipping evaluation of ArTEMiS as the LLM provider is not properly set");
+            return;
+        }
 
         var evaluation = new ArtemisEvaluation(project);
         var result = evaluation.runTraceLinkEvaluation();
         Assertions.assertNotNull(result);
+    }
+
+    private static boolean checkLlmProvision() {
+        // TODO maybe make this more flexible for other settings
+        return Environment.getEnv("OPENAI_API_KEY") == null;
     }
 
 }
