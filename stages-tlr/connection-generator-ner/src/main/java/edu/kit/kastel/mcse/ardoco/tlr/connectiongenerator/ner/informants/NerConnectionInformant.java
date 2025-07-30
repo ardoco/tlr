338fb4b2baade88d13e37ae51d9f33de1a4b4ce7
Jan Keim <jan.keim@kit.edu>
2025-07-30 18:08:13 +0200

    Update artemis algorithm

--- stages-tlr/connection-generator-ner/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/connectiongenerator/ner/informants/NerConnectionInformant.java
+++ stages-tlr/connection-generator-ner/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/connectiongenerator/ner/informants/NerConnectionInformant.java
@@ -1,15 +1,18 @@
 package edu.kit.kastel.mcse.ardoco.tlr.connectiongenerator.ner.informants;
 
 import edu.kit.kastel.mcse.ardoco.core.api.entity.ModelEntity;
+import edu.kit.kastel.mcse.ardoco.core.api.models.Metamodel;
+import edu.kit.kastel.mcse.ardoco.core.api.models.ModelStates;
 import edu.kit.kastel.mcse.ardoco.core.api.stage.connectiongenerator.ner.NamedArchitectureEntity;
 import edu.kit.kastel.mcse.ardoco.core.api.stage.connectiongenerator.ner.NerConnectionState;
+import edu.kit.kastel.mcse.ardoco.core.api.stage.connectiongenerator.ner.NerConnectionStates;
 import edu.kit.kastel.mcse.ardoco.core.common.similarity.SimilarityUtils;
 import edu.kit.kastel.mcse.ardoco.core.common.util.DataRepositoryHelper;
 import edu.kit.kastel.mcse.ardoco.core.data.DataRepository;
 import edu.kit.kastel.mcse.ardoco.core.pipeline.agent.Informant;
 
 public class NerConnectionInformant extends Informant {
-    public static final double DEFAULT_PROBABILITY = 1.0;
+    public static final double DEFAULT_PROBABILITY = 0.92;
     private SimilarityUtils similarityUtils;
 
     public NerConnectionInformant(DataRepository dataRepository) {
@@ -24,14 +27,18 @@ public class NerConnectionInformant extends Informant {
         var modelStatesData = DataRepositoryHelper.getModelStatesData(dataRepository);
         var nerConnectionStates = DataRepositoryHelper.getNerConnectionStates(dataRepository);
         for (var metamodel : modelStatesData.getMetamodels()) {
-            var modelEndpoints = modelStatesData.getModel(metamodel).getEndpoints();
-            var nerConnectionState = nerConnectionStates.getNerConnectionState(metamodel);
-            var namedArchitectureEntities = nerConnectionState.getNamedArchitectureEntities();
-            for (var namedArchitectureEntity : namedArchitectureEntities) {
-                for (var modelEndpoint : modelEndpoints) {
-                    if (areNamedArchitectureEntityAndModelEndpointSimilar(namedArchitectureEntity, modelEndpoint)) {
-                        createTraceLinks(nerConnectionState, namedArchitectureEntity, modelEndpoint);
-                    }
+            processForMetamodel(metamodel, modelStatesData, nerConnectionStates);
+        }
+    }
+
+    private void processForMetamodel(Metamodel metamodel, ModelStates modelStatesData, NerConnectionStates nerConnectionStates) {
+        var modelEndpoints = modelStatesData.getModel(metamodel).getEndpoints();
+        var nerConnectionState = nerConnectionStates.getNerConnectionState(metamodel);
+        var namedArchitectureEntities = nerConnectionState.getNamedArchitectureEntities();
+        for (var namedArchitectureEntity : namedArchitectureEntities) {
+            for (var modelEndpoint : modelEndpoints) {
+                if (areNamedArchitectureEntityAndModelEndpointSimilar(namedArchitectureEntity, modelEndpoint)) {
+                    createTraceLinks(nerConnectionState, namedArchitectureEntity, modelEndpoint);
                 }
             }
         }
@@ -40,12 +47,12 @@ public class NerConnectionInformant extends Informant {
     private boolean areNamedArchitectureEntityAndModelEndpointSimilar(NamedArchitectureEntity namedArchitectureEntity, ModelEntity modelEndpoint) {
         var name = namedArchitectureEntity.getName();
         var modelEndpointName = modelEndpoint.getName();
-        if (similarityUtils.areWordsSimilar(name, modelEndpointName)) {
+        if (similarityUtils.areWordsSimilar(name, modelEndpointName) || similarityUtils.areWordsSimilar(modelEndpointName, name)) {
             return true;
         }
 
         for (var alternativeName : namedArchitectureEntity.getAlternativeNames()) {
-            if (similarityUtils.areWordsSimilar(alternativeName, modelEndpointName)) {
+            if (similarityUtils.areWordsSimilar(alternativeName, modelEndpointName) || similarityUtils.areWordsSimilar(modelEndpointName, alternativeName)) {
                 return true;
             }
         }
