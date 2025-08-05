b8f3a0ffe9082b943be53de03d9b1fce0bdbf425
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-08-05 15:50:34 +0200

    Update ArDoCode values

--- tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/tlr/tests/approach/ArDoCodeEvaluationProject.java
+++ tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/tlr/tests/approach/ArDoCodeEvaluationProject.java
@@ -5,10 +5,10 @@ import edu.kit.kastel.mcse.ardoco.core.tests.eval.ExpectedResults;
 import edu.kit.kastel.mcse.ardoco.tlr.tests.task.DocumentationToCodeTlrTask;
 
 public enum ArDoCodeEvaluationProject {
-    MEDIASTORE(DocumentationToCodeTlrTask.MEDIASTORE, new ExpectedResults(.049, .66, .09, .857, .15, .86)),//
+    MEDIASTORE(DocumentationToCodeTlrTask.MEDIASTORE, new ExpectedResults(.049, .593, .096, .856, .145, .86)),//
     TEASTORE(DocumentationToCodeTlrTask.TEASTORE, new ExpectedResults(.196, .74, .31, .787, .305, .79)),//
-    TEAMMATES(DocumentationToCodeTlrTask.TEAMMATES, new ExpectedResults(.37, .92, .53, .928, .56, .93)),//
-    BIGBLUEBUTTON(DocumentationToCodeTlrTask.BIGBLUEBUTTON, new ExpectedResults(.07, .57, .126, .80, .147, .81)),//
+    TEAMMATES(DocumentationToCodeTlrTask.TEAMMATES, new ExpectedResults(.39, .916, .55, .928, .57, .93)),//
+    BIGBLUEBUTTON(DocumentationToCodeTlrTask.BIGBLUEBUTTON, new ExpectedResults(.08, .56, .14, .805, .156, .81)),//
     JABREF(DocumentationToCodeTlrTask.JABREF, new ExpectedResults(.66, 1.00, .798, .855, .727, .80));
 
     private final DocumentationToCodeTlrTask tlrTask;
