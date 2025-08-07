0669b1d20914b78165b28bd2c736d80565d80adf
Jan Keim <jan.keim@kit.edu>
2025-08-07 09:22:33 +0200

    Update prompt and expected values

--- tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/tlr/tests/approach/ArtemisEvaluationProject.java
+++ tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/tlr/tests/approach/ArtemisEvaluationProject.java
@@ -7,7 +7,7 @@ import edu.kit.kastel.mcse.ardoco.tlr.tests.task.DocumentationToArchitectureMode
 public enum ArtemisEvaluationProject {
     MEDIASTORE(DocumentationToArchitectureModelTlrTask.MEDIASTORE, new ExpectedResults(.995, .865, .925, .985, .925, .995)),//
     TEASTORE(DocumentationToArchitectureModelTlrTask.TEASTORE, new ExpectedResults(.535, .995, .695, .945, .715, .945)),//
-    TEAMMATES(DocumentationToArchitectureModelTlrTask.TEAMMATES, new ExpectedResults(.666, .666, .666, .900, .500, .900)),//
+    TEAMMATES(DocumentationToArchitectureModelTlrTask.TEAMMATES, new ExpectedResults(.42, .660, .666, .900, .500, .900)),//
     BIGBLUEBUTTON(DocumentationToArchitectureModelTlrTask.BIGBLUEBUTTON, new ExpectedResults(.655, .655, .655, .955, .635, .975)),//
     JABREF(DocumentationToArchitectureModelTlrTask.JABREF, new ExpectedResults(.900, .999, .947, .974, .932, .966));
 
